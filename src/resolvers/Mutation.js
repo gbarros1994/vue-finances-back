59b87556ab0719e28d7c66aeac693b738496680d
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2022-02-05 22:29:01 -0300

    add jwt secret and expiresIn

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 async function signup (_, args, ctx, info) {
   const password = await bcrypt.hash(args.password, 10);
   const user = await ctx.db.mutation.createUser({
@@ -9,7 +11,12 @@ async function signup (_, args, ctx, info) {
     }
   });
 
-  const token = jwt.sign({userId: user.id})
+  const token = jwt.sign({userId: user.id}, JWT_SECRET, { expiresIn: '2h' });
+
+  return {
+    token,
+    user
+  }
 }
 
 module.exports = {
